d26358c7a32065b2a7af7a6b4ec254d5ff265e86
srialdythia <srialdythia@gmail.com>
2022-05-16 19:28:10 +0700

    app configuration

--- UseCaseToGUI/UseCaseToGUI/urls.py
+++ UseCaseToGUI/UseCaseToGUI/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('mainApp.urls')), #jika terdapat request,alihkan fungsi UseCaseToGUI urls ke accounts urls
 ]
