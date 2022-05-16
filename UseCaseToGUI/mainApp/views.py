5d50267fe0ee3f7560855630da5a078628a17e13
srialdythia <srialdythia@gmail.com>
2022-05-16 21:41:02 +0700

    add login, logout, & register feature

--- UseCaseToGUI/mainApp/views.py
+++ UseCaseToGUI/mainApp/views.py
@@ -1,12 +1,57 @@
-from django.shortcuts import render,HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages 
 
 # Create your views here.
+from .forms import  CreateUserForm
+
 
 def home(request):
     context={}
     return render(request,'mainApp/dashboard.html')
 
+def registerPage(request):
+    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request, 'mainApp/register.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+        
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'mainApp/login.html', context)
+
 def logoutUser(request):
-    return HttpResponse('logout')
+    logout(request)
+    return redirect('login')
 
 
