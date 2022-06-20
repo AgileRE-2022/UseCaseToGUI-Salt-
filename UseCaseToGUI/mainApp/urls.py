4cf0b19f4fd542e7f7d53321725ab628fc2e0e13
leonardo <lechandra2001@gmail.com>
2022-06-20 22:49:01 +0700

    Change Login Button Redirection

--- UseCaseToGUI/mainApp/urls.py
+++ UseCaseToGUI/mainApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.showUseCaseScenario , name="showUseCaseScenario"),
+    path('', views.showUseCaseScenario , name="home"),
 
     path('register/', views.registerPage , name="register"),
     path('login/', views.loginPage , name="login"),
